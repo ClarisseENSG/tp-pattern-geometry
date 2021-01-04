ea35004d499e5b8a6876dbc4b472e25ad8f406c0
Clarisse Ducatillion <clarisse.ducatillion@gmail.com>
2021-01-04 15:56:14 +0100

    0.2 - Geometry, Point et LineString

---
+++ src/test/java/org/acme/geometry/LineStringTest.java
@@ -0,0 +1,41 @@
+package org.acme.geometry;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LineStringTest {
+	
+public static final double EPSILON = 1.0e-15;
+	
+	@Test
+	public void testDefaultConstructor() {
+		LineString lineString = new LineString();
+		Assert.assertEquals(0, lineString.getNumPoints());
+	}
+	
+	@Test
+	public void testConstructorWithParam() {
+		Coordinate coord = new Coordinate(1.0, -2.0);
+		List<Point> points = new ArrayList<Point>();
+		points.add(new Point(coord));
+		points.add(new Point());
+		
+		LineString lineString = new LineString(points);
+		Assert.assertEquals(2, lineString.getNumPoints());
+		
+		Assert.assertEquals(1.0, lineString.getPointN(0).getCoordinate().getX(), EPSILON);
+		Assert.assertEquals(-2.0, lineString.getPointN(0).getCoordinate().getY(), EPSILON);
+		Assert.assertEquals(0.0, lineString.getPointN(1).getCoordinate().getX(), EPSILON);
+		Assert.assertEquals(0.0, lineString.getPointN(1).getCoordinate().getY(), EPSILON);
+	}
+	
+	@Test
+	public void testGetType() {
+		LineString lineString = new LineString();
+		Assert.assertEquals("LineString", lineString.getType());
+	}
+
+}
---
+++ src/test/java/org/acme/geometry/PointTest.java
@@ -0,0 +1,30 @@
+package org.acme.geometry;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PointTest {
+
+	public static final double EPSILON = 1.0e-15;
+	
+	@Test
+	public void testDefaultConstructor() {
+		Point point = new Point();
+		Assert.assertEquals(0.0, point.getCoordinate().getX(), EPSILON);
+		Assert.assertEquals(0.0, point.getCoordinate().getY(), EPSILON);
+	}
+	
+	@Test
+	public void testConstructorWithParam() {
+		Coordinate coord = new Coordinate(1.0, -2.0);
+		Point point = new Point(coord);
+		Assert.assertEquals(1.0, point.getCoordinate().getX(), EPSILON);
+		Assert.assertEquals(-2.0, point.getCoordinate().getY(), EPSILON);
+	}
+	
+	@Test
+	public void testGetType() {
+		Point point = new Point();
+		Assert.assertEquals("Point", point.getType());
+	}
+}
